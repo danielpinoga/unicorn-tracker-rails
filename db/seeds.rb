8cad1994e0df1ac812365c8d8a87b4a4fb516fb1
danielpinoga <daniel.pino@generalassemb.ly>
2018-07-30 14:02:39 -0400

    adding ffaker for better seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,25 @@ Location.destroy_all
 
 pasture = Location.new(name: 'pasture')
 pasture.unicorns.new(
-  name: 'Danny',
-  color: 'purple'
+  name: FFaker::Name.name,
+  color: FFaker::Color.name
 )
 pasture.save
 
 barn = Location.new(name: 'barn')
 barn.unicorns.new(
-  name: 'Johnson',
-  color: 'green'
+  name: FFaker::Name.name,
+  color: FFaker::Color.name
 )
 barn.save
 
 corral = Location.new(name: 'corral')
-corral.unicorns.new(
-  name: 'Secretly just a horse',
-  color: 'brown'
-)
+
+3.times do
+  corral.unicorns.new(
+    name: FFaker::Name.name,
+    color: FFaker::Color.name
+  )
+end
 corral.save
+
